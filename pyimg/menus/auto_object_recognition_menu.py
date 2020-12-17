fix scale and rotation

pyimg/menus/auto_object_recognition_menu.py
@@ -28,8 +28,8 @@ def scale_transform(image: ImageImpl, scale: Union[float, int]) -> ImageImpl:
     Returns:
         image (np.ndarray): Scaled image
     """
-    _scale = lambda dim, s: int(dim * s / 100)
-    im: image.array
+    _scale = lambda dim, s: int(dim * s)
+    im = image.array
     width, height, channels = im.shape
     new_width: int = _scale(width, scale)
     new_height: int = _scale(height, scale)
@@ -40,7 +40,7 @@ def scale_transform(image: ImageImpl, scale: Union[float, int]) -> ImageImpl:
 def rotate_transform(image: ImageImpl, angle: Union[float, int]) -> ImageImpl:
     image_center = tuple(np.array(image.array.shape[1::-1]) / 2)
     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-    result = cv2.warpAffine(image, rot_mat, image.array.shape[1::-1], flags=cv2.INTER_LINEAR)
+    result = cv2.warpAffine(image.array, rot_mat, image.array.shape[1::-1], flags=cv2.INTER_LINEAR)
     return ImageImpl.from_array(result)
 
 
@@ -58,7 +58,7 @@ def automated_result(
     similarities_color = ['r', 'g', 'b', 'y']
 
     illumination_range = range(-30, 30, 5)
-    scaling_range = range(-80, 80, 20)
+    scaling_range = [x * 0.1 for x in range(5, 15)]
     rotation_range = range(0, 330, 30)
 
     fig = plt.figure()